8c8d820c4651e5ba39b0942e7acd34a9cea18313
Dwi Satrio W <31796624+dwisatriow@users.noreply.github.com>
2023-07-29 22:16:05 +0700

    create a slice exercise

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { tasksSlice } from './tasksSlice';
+import { humansSlice } from './humansSlice';
 
 export const store = configureStore({
   reducer: {
     tasks: tasksSlice.reducer,
-    humans: (state) => []
+    humans: humansSlice.reducer
   }
 });
