ad0fe9d16b17b3d0dfb41b6b6ea3c66ae218bf24
Dwi Satrio W <31796624+dwisatriow@users.noreply.github.com>
2023-07-29 19:11:21 +0700

    creating a slice of state

---
+++ src/store/tasksSlice.js
@@ -0,0 +1,23 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const createTask = (title) => ({
+  id: nanoid(),
+  title,
+  completed: false,
+  assignedTo: ''
+});
+
+const initialState = [
+  createTask('Order more energy drinks'),
+  createTask('Water the plants')
+];
+
+export const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    add: (state, action) => {
+      state.push(createTask(action.payload));
+    }
+  }
+});
